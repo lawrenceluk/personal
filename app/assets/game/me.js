94c7f3c27e2732970c19ffb55dd5ce2dcafd2dcc
lawrenceluk <lluk@ucsd.edu>
2014-10-01 15:30:04 -0700

    Auto hover for mobile

--- app/assets/game/me.js
+++ app/assets/game/me.js
@@ -1,3 +1,28 @@
 $("#me-picture").click(function() {
-	
+
+});
+
+/* 
+ * http://stackoverflow.com/questions/5353934/check-if-element-is-visible-on-screen
+ */
+function checkVisible(elm, evalType) {
+    evalType = evalType || "visible";
+
+    var vpH = $(window).height(), // Viewport Height
+        st = $(window).scrollTop() - 280, // Scroll Top
+        y = $(elm).offset().top,
+        elementHeight = $(elm).height() - vpH;
+
+    if (evalType === "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+    if (evalType === "above") return ((y < (vpH + st)));
+}
+
+$(window).scroll(function () {
+	$(".p-item").each(function() {
+		if (checkVisible($(this), "visible")) {
+			$(this).addClass("p-overlay");
+		} else {
+			$(this).removeClass("p-overlay");
+		}
+	});
 });
